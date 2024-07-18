428d6dccd026cece9a4a625f59693d07c0d9ca60
Waheeda <waheeda.ias@gmail.com>
2024-07-18 15:42:02 +0530

    added list and map

--- 03_variables/vars.tf
+++ 03_variables/vars.tf
@@ -10,7 +10,20 @@ variable "z" {
   default = "Hello Waheeda"
 }
 
-#valyes numbers and boolean need tot be quoted only strings are quoted
+#List variable
+#Values in list need not be of same data type
+variable "l" {
+  default = [10,20,40,"abc",false]
+}
+#values numbers and boolean need tot be quoted only strings are quoted
+
+#Map variable
+variable "m" {
+  default = {
+    cousre = "Devops"
+    Trainer = "Raghu"
+  }
+}
 
 #outputs
 #direct values does nor require to be accessed with ${}
@@ -21,4 +34,12 @@ output "x" {
 #Accessing variable has a combination with a string then we need to refer the variable with ${}
 output "x1" {
   value = "Value of x- ${var.x}"
+}
+
+output "l" {
+  value = "var.l[1]"
+}
+
+output "m" {
+  value = "Course Name:- ${var.m["course"]}, Trainer Name:- ${var.m["trainer"]}"
 }
