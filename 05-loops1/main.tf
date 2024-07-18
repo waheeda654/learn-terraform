de2ab0ed9dbe2b9bbda14b5cb2d45455e204a847
Waheeda <waheeda.ias@gmail.com>
2024-07-18 16:38:31 +0530

    null resoiurce

--- 05-loops1/main.tf
+++ 05-loops1/main.tf
@@ -1,3 +1,11 @@
 resource "null_resource" "test" {
   count = 3
 }
+
+variable "components" {
+  default = ["frontend","catalogue","mongo"]
+}
+
+resource "null_resource" "test" {
+  count = length(var.components)
+}
