83b66a0a85a5f5420e97a20a24e5889697c3f353
Waheeda <waheeda.ias@gmail.com>
2024-07-18 15:34:02 +0530

    Terrafrom code - added ouput dir

--- 03_variables/vars.tf
+++ 03_variables/vars.tf
@@ -2,7 +2,23 @@ variable "x" {
   default = 10
 }
 
+variable "y" {
+  default = true
+}
+
+variable "z" {
+  default = "Hello Waheeda"
+}
+
+#valyes numbers and boolean need tot be quoted only strings are quoted
+
 #outputs
+#direct values does nor require to be accessed with ${}
 output "x" {
   value = var.x
+}
+
+#Accessing variable has a combination with a string then we need to refer the variable with ${}
+output "x1" {
+  value = "Value of x- ${var.x}"
 }
