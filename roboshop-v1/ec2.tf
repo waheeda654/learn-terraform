0284e8060d00b193415fd0933e79bb99e6cdaeea
Waheeda <waheeda.ias@gmail.com>
2024-07-18 13:49:07 +0530

    Terrafrom code - added zone for mongo and catalogue

--- roboshop-v1/ec2.tf
+++ roboshop-v1/ec2.tf
@@ -24,6 +24,14 @@ resource "aws_instance" "mongo" {
   }
 }
 
+resource "aws_route53_record" "mongo" {
+  zone_id = "Z0945346ECJX5XF9YH9W"
+  name    = "mongo.dev.wdevops.fun"
+  type    = "A"
+  ttl     = 15
+  records = [aws_instance.mongo.private_ip]
+}
+
 resource "aws_instance" "catalogue" {
   ami           = "ami-041e2ea9402c46c32"
   instance_type = "t3.small"
@@ -32,4 +40,12 @@ resource "aws_instance" "catalogue" {
   tags = {
     Name = "catalogue.dev"
   }
+}
+
+resource "aws_route53_record" "catalogue" {
+  zone_id = "Z0945346ECJX5XF9YH9W"
+  name    = "catalogue.dev.wdevops.fun"
+  type    = "A"
+  ttl     = 15
+  records = [aws_instance.catalogue.private_ip]
 }
