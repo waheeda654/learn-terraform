1df5de37372eac2adbcb1566f590e7b3b0817a90
Waheeda <waheeda.ias@gmail.com>
2024-07-19 10:48:01 +0530

    null resoiurce

---
+++ roboshop-v4/main.tf
@@ -0,0 +1,18 @@
+
+resource "aws_instance" "instance" {
+  count = length(var.components)
+  ami           = data.aws_ami.ami.image_id
+  instance_type = var.instance_type
+  vpc_security_group_ids =data.aws_security_groups.sg.ids
+  tags = {
+    Name = "${var.components[count.index]}.dev"
+  }
+}
+resource "aws_route53_record" "dns_record" {
+  count = length(var.components)
+  zone_id = data.aws_route53_zone.zone.zone_id
+  name    = "frontend.dev.${var.domain_name}"
+  type    = "A"
+  ttl     = 15
+  records = [aws_instance.instance[count.index].private_ip]
+}
